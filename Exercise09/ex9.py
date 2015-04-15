96db7e658274c4cb16eaa514d0911ed800bd7cd7
Edward <feek1g@gmail.com>
2015-04-15 08:24:49 -0700

    completed exercise 9

---
+++ Exercise09/ex9.py
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remember type it exactly.
+
+#creates variable days which holds a string as a value
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#creates a variable months which hold string values along with the new line character'
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+#prints statement followed by the value of days
+print "Here are the days: ", days
+#prints statement followed by the value of months
+print "Here are the months: ", months
+
+# """ is used to delimit the string.
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
