9307ba600effe2099f5bb82e79d0c85015e9bfa1
Edward <feek1g@gmail.com>
2015-04-15 08:10:04 -0700

    completed exercise 7

---
+++ Exercise07/ex7.py
@@ -0,0 +1,26 @@
+#prints statement
+print "Mary hash a little lamb."
+#prints statement and subs %s with snow
+print "Its fleece was white as %s." % 'snow'
+#prints statement
+print "And everywhere that Mary went."
+#prints statement 10 tims
+print "." * 10
+
+#assigns variable end(N) with a a string value.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#variables are concatinated to print the word "Cheese Burger".
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
