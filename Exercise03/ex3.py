14cc77ad9568caf38e866e208e8b3994688405f9
Edward <feek1g@gmail.com>
2015-04-14 23:54:21 -0700

    completed exercise 2 and 3

---
+++ Exercise03/ex3.py
@@ -0,0 +1,29 @@
+#print statement
+print "I will now count my chickens:"
+
+# prints Hen count, by doing addition then division
+print "Hens", 25 + 30.0 / 6
+# prints Rooster count by doing subtration, multiplication, and modulus.
+print "Roosters", 100.0 - 25 * 3 % 4
+#print statement
+print "Now I will count the eggs:"
+#prints egg count, by doing addition, subtraction, and modulus.
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#prints statement
+print "Is it true that 3.0 + 2 < 5 - 7?"
+# Compares if the left side is lesser than the right side, returning true or false value
+print 3.0 + 2 < 5 - 7
+#prints statement, then after the ',' python perfoms addition.
+print "What is 3.0 + 2?", 3 + 2
+#prints statement, then after the ',' python perfoms subtraction.
+print "What is 5.0 - 7?", 5 - 7
+#prints statement
+print "Oh, that's why it's False."
+#print statement
+print "How about some more."
+#prints statement, then compares if the left side is greater than the right side, returning a true or false value
+print "Is it greater?", 5.0 > -2
+#prints statement, then compares if the left side is greater than or equalto the right side, returning a true or false value
+print "Is it greater or equal?", 5.0 >= -2
+#prints statement, then compares if the left side is lesser than or equal to the right side, returning a true or false value
+print "Is it less or equal?", 5.0 <= -2
