0f84426c319503e01f9e8859873b677ec3be59f4
Edward <feek1g@gmail.com>
2015-04-22 13:50:57 -0700

    completed ex28

---
+++ Exercise28/ex28.py
@@ -0,0 +1,61 @@
+# Exercise 28 - Boolean Practice
+
+# true
+True and True
+
+#false
+False and True
+
+#false
+1 == 1 and 2 == 1
+
+#true
+"test" == "test"
+
+#true
+1 == 1 or 2 != 1
+
+#true
+True and 1 == 1
+
+#false
+False and 0 != 0
+
+#true
+True or 1 == 1
+
+#false
+"test" == "testing"
+
+#false
+1 != 0 and 2 == 1
+
+#true
+"test" != "testing"
+
+#false
+"test" == 1
+
+#true
+not (True and False)
+
+#false
+not (1 == 1 and 0 != 1)
+
+#false
+not (10 == 1 or 1000 == 1000)
+
+#false
+not (1 != 10 or 3 == 4)
+
+#true
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+
+#true
+1 == 1 and (not ("testing" == 1 or 1 == 0))
+
+#false
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+
+#false
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
