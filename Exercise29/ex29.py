6939de666a51d61cef37e5177c72df60ba8ca5de
Edward <feek1g@gmail.com>
2015-04-22 14:28:28 -0700

    completed Exercise29

--- Exercise29/ex29.py
+++ Exercise29/ex29.py
@@ -1,3 +1,5 @@
+#Exercise 29 - What if
+
 people = 20
 cats = 30
 dogs = 15
@@ -27,3 +29,15 @@ if people <= dogs:
 
 if people == dogs:
     print "People are dogs."
+
+
+#What do you think the if does to the code under it?
+# The code will execute if the block yields true
+#Why does the code under the if need to be indented four spaces?
+# It's pythons way of seperating another block of code.
+#What happens if it isn't indented?
+# Python Indentation Error
+#Can you put other boolean expressions from Exercise 27 in the if-statement? Try it.
+# Yes you can.
+#What happens if you change the initial values for people, cats, and dogs?
+# The results will vary depending if it's a truthy or falsey.
