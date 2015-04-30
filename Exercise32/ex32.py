275c3b429978db520805f0551c0cad4fd3592fde
Edward <feek1g@gmail.com>
2015-04-29 19:08:34 -0700

    adds fruits and mixed list

--- Exercise32/ex32.py
+++ Exercise32/ex32.py
@@ -7,3 +7,12 @@ change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 # this first kind of for-loop goes through list
 for number in the_count:
     print "This is the count %d" % number
+
+#same as above
+for fruit in fruits:
+    print "A fruit type of %s" % fruit
+
+# also we can go through mixed lists too
+# notice we have to use %r wince we don't know what's in it
+for i in change:
+    print "I got %r" % i
