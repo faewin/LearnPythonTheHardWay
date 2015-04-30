612f2878f56e9ae26b7b27c94a02bb8b009ac170
Edward <feek1g@gmail.com>
2015-04-29 19:38:31 -0700

    completed study drill

--- Exercise33/ex33.py
+++ Exercise33/ex33.py
@@ -1,16 +1,19 @@
 # Exercise 32 - While loops
 
-i = 0
-numbers = []
+def method(max_num, inc):
+    i = 0
+    numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+    while i < max_num:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i += 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        i += inc
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
 
-print "The numbers: "
-for num in numbers:
-    print num
+    print "The numbers: "
+    for num in numbers:
+            print num
+
+method(6,1)
