10b3b8e7340533e5dc6e3f1c0243406fbc38bdd8
Edward <feek1g@gmail.com>
2015-04-29 19:04:06 -0700

    adds first loop

--- Exercise32/ex32.py
+++ Exercise32/ex32.py
@@ -4,4 +4,6 @@ the_count = [1,2,3,4,5]
 fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
-
+# this first kind of for-loop goes through list
+for number in the_count:
+    print "This is the count %d" % number
