775d8ffafd4175022cd3458f2317fa764bcbfccb
Edward <feek1g@gmail.com>
2015-04-29 19:54:31 -0700

    completed challenge

---
+++ Exercise34/ex34.py
@@ -0,0 +1,10 @@
+# Exercise 34 -  Accessing Elements of List
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# the animal bear     is at animals[0]
+# the animal python   is at animals[1]
+# the animal peacock  is at animals[2]
+# the animal kangaroo is at animals[3]
+# the animal whale    is at animals[4]
+# the animal platypus is at animals[5]
