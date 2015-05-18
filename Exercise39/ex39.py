a426b5106bd45e6a73e92539416111c866bede1a
Edward <feek1g@gmail.com>
2015-05-17 23:38:26 -0700

    adds file

--- Exercise39/ex39.py
+++ Exercise39/ex39.py
@@ -8,9 +8,35 @@ print person['city']
 
 # create a mapping of state to abbreviation
 states = {
-        'Oregon': 'OR',
-        'Florida': 'FL',
-        'California': 'CA',
-        'New York': 'NY',
-        'Michigan': 'MI'
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
 }
+
+# create a basic set of states and cities in them
+
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+# add more cities
+
+cities['NY'] = 'New York'
+cities['TX'] = 'Austin'
+cities['OR'] = 'Oregon'
+
+# print cities
+
+print "-" * 10
+print "NY state has: ", cities['NY']
+print "OR state has: ", cities['OR']
+
+# print some states
+print "-" * 10
+print "Florida's abbreviation is: ", states['Florida']
+print "New Yorks's abbreviation is: ", states['New York']
+
