419292f4fd47b7fb97cf7643006fdba6a75b58f6
Edward <feek1g@gmail.com>
2015-05-18 14:27:16 -0700

    looping though hash

--- Exercise39/ex39.py
+++ Exercise39/ex39.py
@@ -26,7 +26,6 @@ cities = {
 # add more cities
 
 cities['NY'] = 'New York'
-cities['TX'] = 'Austin'
 cities['OR'] = 'Oregon'
 
 # print cities
@@ -40,3 +39,29 @@ print "-" * 10
 print "Florida's abbreviation is: ", states['Florida']
 print "New Yorks's abbreviation is: ", states['New York']
 
+# print every state abbreviation
+print '-' * 10
+for state, abbrev in states.items():
+    print "%s is abbreviated %s" % (state, abbrev)
+
+# print every city in state
+print '-' * 10
+for abbrev, city in cities.items():
+    print "%s has the city %s" % (abbrev, city)
+
+# now do both at the same time
+print '-' * 10
+for state, abbrev in states.items():
+    print "%s state is abbreviated %s and has city %s" % (
+        state, abbrev, cities[abbrev])
+
+print '-' * 10
+# safely get a abbreviation by state that might not be there
+state = states.get('Texas')
+
+if not state:
+    print "Sorry, no Texas."
+
+# get a city with a default value
+city = cities.get('TX', 'Does Not Exist')
+print "The city for the state 'TX' is: %s" % city
