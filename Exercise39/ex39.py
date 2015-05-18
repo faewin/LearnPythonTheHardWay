c70c02cf9117422b0b2e083803ac5be11acf8e15
Edward <feek1g@gmail.com>
2015-05-17 19:43:58 -0700

    adds ex39

---
+++ Exercise39/ex39.py
@@ -0,0 +1,7 @@
+# Exercise 39 - Dictionaries, Oh lovely dictionaries
+
+person = {'name': 'Edward', 'age': 25}
+print person['name']
+print person['age']
+person['city'] = 'San Francisco'
+print person['city']
