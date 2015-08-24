3e09a65de7b914de5636d8eb7a51b7658e759ca4
Edward Frias <feek1g@gmail.com>
2015-08-24 06:26:10 -0700

    explains binary reprensentation

--- Extra/csp01.py
+++ Extra/csp01.py
@@ -4,13 +4,23 @@
 # > problem by the use of the *Representation*
 
 # ♦ Explain what a computer algorithm is
-# > A set of an instructions used to solve problem. But there are different
-# > levels of algorithms, ideally you want an algorithms that that correctly
+# > A set of an instructions used to solve problem or goal. But there are different
+# > levels of algorithms, ideally you want an algorithms that correctly
 # > solves a problem in a reasonable amount of time or othewise it is of
 # > limited used in practical use.
 
 # ♦ Explain the fundamental components of digital hardware
-# ♦ Explain the role of binary representation in digital computing ♦ Explain what an operating systems is
+# > CPU - The processing unit for a computer
+# > RAM - The memory of a computer where executed programs reside
+# > Motherboard - Buses that transfer data between the CPU and Memory
+# > Storage - HDD, SSD, USB-Stick
+
+# ♦ Explain the role of binary representation in digital computing
+# > Numbers can be defined in any radix base. We usually use base 10 numbers
+# > which are numbers from 0..9 but a binary reprentation uses a radix base of 2
+# > which consists of 0 and 1. Example => base10 - 99 || base2 - 01100011
+
+# ♦ Explain what an operating systems is
 # ♦ Explain the fundamental concepts of computer software
 # ♦ Explain the fundamental features of IDLE in Python
 # ♦ Modify and execute a simple Python program
